54ee3ee09bf1ab605be8a189b7633e54b6b77e70
Blake Engquist <blakeengquist@gmail.com>
2026-01-31 17:31:37 -0600

    removed default db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,11 +15,11 @@ import { AuthModule } from './auth/auth.module';
       useFactory: (configService: ConfigService) => {
         const config = {
           type: 'postgres' as const,
-          host: configService.get('POSTGRES_HOST') || 'host.docker.internal',
-          port: +(configService.get('POSTGRES_PORT') || 5432),
-          username: configService.get('POSTGRES_USER') || 'postgres',
-          password: configService.get('POSTGRES_PASSWORD') || 'password',
-          database: configService.get('POSTGRES_DB') || 'todo',
+          host: configService.get('POSTGRES_HOST'),
+          port: +configService.get('POSTGRES_PORT'),
+          username: configService.get('POSTGRES_USER'),
+          password: configService.get('POSTGRES_PASSWORD'),
+          database: configService.get('POSTGRES_DB'),
           entities: [__dirname + '/**/*.entity.{ts,js}'],
           synchronize: true,
         };
